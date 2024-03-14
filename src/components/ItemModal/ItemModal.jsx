259b082e9125c2baa2e5af23cb0bdf2e17877012
Trang T <trangmtruong@yahoo.com>
2024-03-13 17:54:24 -0700

    add garment images

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -3,9 +3,11 @@ function ItemModal({ activeModal, onClose, card }) {
   return (
     <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
       <div className="modal__content modal__content_type_image">
-        <button onClick={onClose} className="modal__close" type="button">
-          CLOSE
-        </button>
+        <button
+          onClick={onClose}
+          className="modal__close"
+          type="button"
+        ></button>
         <img src={card.link} alt="" className="modal__image" />
         <div className="modal__footer">
           <h2 className="modal__caption">{card.name}</h2>
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -11,9 +11,11 @@ function ModalWithForm({
     <div className={`modal ${activeModal === "add-garment" && "modal_opened"}`}>
       <div className="modal__content">
         <h2 className="modal__title">{titleText}</h2>
-        <button onClick={onClose} className="modal__close" type="button">
-          CLOSE
-        </button>
+        <button
+          onClick={onClose}
+          className="modal__close"
+          type="button"
+        ></button>
         <form className="modal__form">
           {children}
           <button type="submit" className="modal__submit">
