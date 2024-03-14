6bb628b954e8762fb1f37c512688d57f52bd2e53
Trang T <trangmtruong@yahoo.com>
2024-03-14 11:00:20 -0700

    fix item modal close btn and add garment btn

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -19,7 +19,7 @@ function ModalWithForm({
         <form className="modal__form">
           {children}
           <button type="submit" className="modal__submit">
-            {buttonText}
+            {}
           </button>
         </form>
       </div>
